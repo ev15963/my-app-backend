b56662258b47418646c87e3d83c84e400b858533
gpe-joshua <gpechoijs@gmail.com>
2020-01-29 11:28:13 +0900

    no msg

---
+++ src/main/resources/static/precache-manifest.cb44eb917a8b0b1e7a8cd7b24232a284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86d34ce4505643c131b0b2c2736e483",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a706bc63daadbbd95405",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b5e96bd6374e1a06b2bf",
+    "url": "/static/js/2.573eda24.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.573eda24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a706bc63daadbbd95405",
+    "url": "/static/js/main.62787d4b.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  }
+]);
